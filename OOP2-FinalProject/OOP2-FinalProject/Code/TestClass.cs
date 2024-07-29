a5c7debdae063bd5de59e1f9c54114202cfd2215
Manraj Kambo <Manraj.Kambo@edu.sait.ca>
2024-07-29 17:31:38 -0600

    Created Test Page
    Use for reference.

---
+++ OOP2-FinalProject/OOP2-FinalProject/Code/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2_FinalProject.Code
+{
+	public class TestClass
+	{
+		public TestClass() { }
+
+		public static string TestFunc()
+		{
+			return "Test output";
+		}
+
+	}
+}
