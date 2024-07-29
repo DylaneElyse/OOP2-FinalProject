64faeec835a5297d97c3c3ab19b8341891eccd81
DylaneElyse <dylanecunningham@outlook.com>
2024-07-29 17:09:58 -0600

    Project Creation

---
+++ OOP2-FinalProject/OOP2-FinalProject/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP2_FinalProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OOP2-FinalProject/OOP2-FinalProject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OOP2_FinalProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
